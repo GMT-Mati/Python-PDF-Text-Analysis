21e9160acddf459a0ac94621b40e311e082707af
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2024-06-23 06:53:14 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,10 @@ def test_count_word_frequency():
     expected_result = {"test": 3, "only": 1}
     result = count_word_frequency(text)
     assert result == expected_result
+
+def test_count_word_frequency_no_stopwords():
+    text = "This is a test. This test is only a test."
+    # Expected result without filtering stop words
+    expected_result = {"this": 2, "is": 2, "a": 2, "test": 3, "only": 1}
+    result = count_word_frequency(text, filter_stop_words=False)
+    assert result == expected_result
