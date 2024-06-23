027e3eeb7c6cfa2447619a8dbc23bb44f1789c01
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2024-06-23 06:44:52 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import pytest
 from app import count_word_frequency
 
 def test_count_word_frequency():
-    text = "Hello world! Hello everyone."
-    expected_result = {"hello": 2, "world": 1, "everyone": 1}
-    result = count_word_frequency(text, filter_stop_words=False)
+    text = "This is a test. This test is only a test."
+    expected_result = {"this": 2, "test": 3, "only": 1}
+    result = count_word_frequency(text)
     assert result == expected_result
