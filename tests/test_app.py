ced05188f4286eeaab889db342b175ce7256eb9d
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2024-06-23 06:48:34 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ from app import count_word_frequency
 
 def test_count_word_frequency():
     text = "This is a test. This test is only a test."
-    expected_result = {"this": 2, "test": 3, "only": 1}
+    # Expected result with stop words filtered out
+    expected_result = {"test": 3, "only": 1}
     result = count_word_frequency(text)
     assert result == expected_result
